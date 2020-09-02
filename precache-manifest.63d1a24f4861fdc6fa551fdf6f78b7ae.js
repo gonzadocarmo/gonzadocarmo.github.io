5ab65afe68785b7fe1a52ecd953a780521bc6017
do Carmo Norte, Gonzalo <Gonzalo.DoCarmoNorte@sabre.com>
2020-09-01 23:00:29 -0500

    Updates

---
+++ precache-manifest.63d1a24f4861fdc6fa551fdf6f78b7ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8762dcf1b23179a0575ef40056cef35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d983e81f5d18ebfe0cf4",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "e40c6b205db1e8ec23c0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d983e81f5d18ebfe0cf4",
+    "url": "/static/js/2.7a4b70d1.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.7a4b70d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e40c6b205db1e8ec23c0",
+    "url": "/static/js/main.5e7033c2.chunk.js"
+  },
+  {
+    "revision": "c1d2a415cb2925a35655",
+    "url": "/static/js/runtime-main.2638f72f.js"
+  }
+]);
